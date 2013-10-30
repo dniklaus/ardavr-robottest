932fc5d592598ca04b77e8cfaf5dbd0833332a2a
Dieter Niklaus <dieter.niklaus@gmx.net>
2013-10-30 19:46:30 +0100

    - NEW: included Ultrasonic Ranging Driver and NewPing lib.

--- ArdAvr_RobotTest.cpp
+++ ArdAvr_RobotTest.cpp
@@ -5,10 +5,13 @@
 #include "TimerContext.h"
 #include "SN754410Driver.h"
 #include "MotorPWM.h"
+#include "UltrasonicSensor.h"
+#include "UltrasonicSensorHCSR04.h"
 
 LcdKeypad lcdKeypad;
 MotorPWM* motorL;
 MotorPWM* motorR;
+UltrasonicSensor* ultrasonicSensorFront;
 
 
 bool isRunning = false;
@@ -25,19 +28,26 @@ int motor3APin = 47;
 int motor2APin = 48;
 int motor4APin = 49;
 
-
 // value for motor speed
 int speed_value_motor = 0;
 bool isSpeedIncreasing = true;
 bool isFwd = true;
+bool isObstacleDetected = false;
 
 // LCD Backlight Intensity
 int lcdBackLightIntensity = 100;
 
 
+// Ultrasonic Sensor
+unsigned int triggerPin = 34;
+unsigned int echoPin    = 36;
+unsigned long dist      = 0;   // [cm]
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
+  ultrasonicSensorFront = new UltrasonicSensorHCSR04(triggerPin, echoPin);
+
   motorL = new SN754410_Driver(speedPin1, motor1APin, motor2APin);
   motorR = new SN754410_Driver(speedPin2, motor3APin, motor4APin);
 
@@ -46,6 +56,9 @@ void setup()
 
 void speedControl()
 {
+  dist = ultrasonicSensorFront->getDistanceCM();
+  isObstacleDetected = isFwd && (dist > 0) && (dist < 10);
+
   if (lcdKeypad.isRightKey())
   {
     isRunning = false;
@@ -57,7 +70,7 @@ void speedControl()
 
   if (isRunning)
   {
-    if (speed_value_motor == 255)
+    if ((speed_value_motor == 255) || isObstacleDetected)
     {
       isSpeedIncreasing = false;
     }
@@ -66,11 +79,11 @@ void speedControl()
       isSpeedIncreasing = true;
       isFwd = !isFwd;
     }
-    if (isSpeedIncreasing)
+    if (isSpeedIncreasing && (speed_value_motor < 255))
     {
       speed_value_motor++;
     }
-    else
+    else if ((speed_value_motor > 0))
     {
       speed_value_motor--;
     }
@@ -79,7 +92,14 @@ void speedControl()
   {
     if (speed_value_motor > 0)
     {
-      speed_value_motor--;
+      if (isObstacleDetected)
+      {
+        speed_value_motor -= 4;
+      }
+      else
+      {
+        speed_value_motor--;
+      }
     }
   }
 }
@@ -101,16 +121,20 @@ void lcdBackLightControl()
 void updateDisplay()
 {
   lcdKeypad.setCursor(0, 0);
-  lcdKeypad.print("in: ");
-  lcdKeypad.print(lcdKeypad.isNoKey()     ? "NO_KEY    " :
-                  lcdKeypad.isUpKey()     ? "UP_KEY    " :
-                  lcdKeypad.isDownKey()   ? "DOWN_KEY  " :
-                  lcdKeypad.isLeftKey()   ? "LEFT_KEY  " :
-                  lcdKeypad.isRightKey()  ? "RIGHT_KEY " :
-                  lcdKeypad.isSelectKey() ? "SELECT_KEY" : "----------");
+//  lcdKeypad.print("in: ");
+//  lcdKeypad.print(lcdKeypad.isNoKey()     ? "NO_KEY    " :
+//                  lcdKeypad.isUpKey()     ? "UP_KEY    " :
+//                  lcdKeypad.isDownKey()   ? "DOWN_KEY  " :
+//                  lcdKeypad.isLeftKey()   ? "LEFT_KEY  " :
+//                  lcdKeypad.isRightKey()  ? "RIGHT_KEY " :
+//                  lcdKeypad.isSelectKey() ? "SELECT_KEY" : "----------");
+  lcdKeypad.print("Dist:  ");
+  lcdKeypad.print(dist > 99 ? "" : dist > 9 ? " " : "  ");
+  lcdKeypad.print(dist);
+  lcdKeypad.print(" [cm]");
 
   lcdKeypad.setCursor(0, 1);
-  lcdKeypad.print("speed: ");
+  lcdKeypad.print("Speed: ");
   lcdKeypad.print(speed_value_motor > 99 ? "" : speed_value_motor > 9 ? " " : "  ");
   lcdKeypad.print(speed_value_motor);
   lcdKeypad.setCursor(11, 1);
